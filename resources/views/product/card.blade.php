679f9a39f8d80869bbbeeba00bce7771e36a6fc0
Gonzalo Aguero <gonzaloaguerodev@gmail.com>
2023-03-03 12:32:41 -0300

    first commit

---
+++ resources/views/product/card.blade.php
@@ -0,0 +1,16 @@
+<div class="bg-gray-light text-black w-40">
+    <div class="shrink-0">
+        <img class="h-40 w-full" src="{{ Vite::asset('resources/images/defualtImage.svg') }}" alt="Product Thumbnail">
+    </div>
+    <h3 class="text-center text-sm font-medium"><a>Cerámicos Marca Modelo x 2,64m2</a></h3>
+    <div class="text-center font-light flex-col items-center">
+        <!-- Primary price -->
+        <span class="text-base">$2399</span>
+        <!-- Secondary price -->
+        <span class="text-xs">$904/caja</span>
+    </div>
+    <!-- Add to cart functionalities -->
+    <div class="flex w-full justify-center pt-2">
+        <button class="bg-orange text-white text-sm p-1">Añadir al carrito</button>
+    </div>
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,54 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        screens: {
+            sm: '480px',
+            md: '768px',
+            lg: '1024px',
+            xl: '1280px',
+            "2xl": '1536px'
+        },
+        colors: {
+            'blue': '#1fb6ff',
+            'purple': '#7e5bef',
+            'pink': '#ff49db',
+            'orange': '#ffb93a',
+            'green': '#13ce66',
+            'yellow': '#ffc82c',
+            'gray-dark': '#273444',
+            'gray': '#8492a6',
+            'gray-light': '#fafafa',
+            'white': '#ffffff',
+            'black':'#0c0c0c',
+        },
+        fontFamily: {
+            sans: ['Inter var','Graphik', 'sans-serif'],
+            serif: ['Merriweather', 'serif'],
+        },
+        extend: {
+            spacing: {
+                '128': '32rem',
+                '144': '36rem',
+            },
+            borderRadius: {
+                '4xl': '2rem',
+            }
+        },
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: '1rem',
+                sm: '2rem',
+                lg: '4rem',
+                xl: '5rem',
+                '2xl': '6rem',
+            },
+        },
+    },
+    plugins: [],
+}
