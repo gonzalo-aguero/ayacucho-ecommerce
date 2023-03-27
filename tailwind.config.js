f8adde00f26bad16f5ec2ba692d606bdd47ee694
Gonzalo Aguero <gonzaloaguerodev@gmail.com>
2023-03-27 16:58:14 -0300

    Responsive design

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
         screens: {
             sm: '480px',
             md: '768px',
+            "md-820":  "820px",
             lg: '1024px',
             xl: '1280px',
             "2xl": '1536px'
